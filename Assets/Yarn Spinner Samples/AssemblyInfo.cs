a3de20433488efbd697f5afefd26bf1f7aa64e5d
julian-gosiengfiao <juchgo@gmail.com>
2025-03-05 11:43:44 +0100

    project add, player no longer sees "Allow me to explain." twice.

---
+++ Assets/Yarn Spinner Samples/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿/*
+Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
+*/
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.5.2.0")]
+[assembly: AssemblyFileVersion("2.5.2.0")]
+[assembly: AssemblyInformationalVersion("2.5.2-assetstore.1+0.Branch.assetstore.Sha.42f84a924e627e19a1d3e97c181b04f2f56e2d05")]
